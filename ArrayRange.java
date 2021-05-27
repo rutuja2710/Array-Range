4473c94f6402d7883e74e1ff42993de56e65480f
rutuja2710 <55893593+rutuja2710@users.noreply.github.com>
2021-05-27 15:01:19 +0530

    Create ArrayRange.java

---
+++ ArrayRange.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArrayRange {
+
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+        int arraySize=scanner.nextInt();
+        
+        int inputArray[] = new int[arraySize];
+        for(int i=0;i<arraySize;i++)
+            inputArray[i]=scanner.nextInt();
+        
+        Arrays.sort(inputArray);
+        
+        int range = inputArray[arraySize-1]-inputArray[0];
+        
+        System.out.println("The range of the array is "+ range);
+        
+    }
+}
